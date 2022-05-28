40a8205ffa440e277de91eefa5211c6deae4ecd0
DmitriiMarakulin <marakulindn@gmail.com>
2022-05-28 20:59:06 +0800

    GG

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class GameLauncher {
     public static void main(String[] args)  {
-        Scanner scanner = new Scanner(System.in);
-        GuessGame game = new GuessGame();
+                GuessGame game = new GuessGame();
                   game.startGame(1, 5);
 
     }
